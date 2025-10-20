109389972d9577d765d3be6e5a6deb9527bc1d1f
Jirrer <jrirrer@gmail.com>
2025-10-19 20:34:44 -0400

    made progress on pulling purchase info

--- backend/ParsePDFs.py
+++ backend/ParsePDFs.py
@@ -1,8 +1,10 @@
 from Classes import Purchase
 from MiscMethods import isDate
 from pypdf import PdfReader
+import re
 
-PaymentDefinitionConfidence = 1.0
+# Issue - skipped netflix because it did not have a card number with it
+
 
 def main():
     purchases = getPurchases(["testPdf"])
@@ -13,14 +15,11 @@ def main():
 
 
 def getPurchases(pdfsArray): 
-    rawPurchases = []
-    for pdf in pdfsArray:
-        rawPurchases.append(parsePdf(pdf))
-
+    rawPurchases = parsePdf(pdfsArray)
 
     purchases = []
     for p in rawPurchases:
-        purchases.append(categorisePurchase(p))
+        purchases.append(categorisePurchase(p)) # < ready to start categorizing
 
     return purchases
     
@@ -40,33 +39,17 @@ def parsePdf(inputPDFs):
 
             pdfContent.append(page.extract_text())
 
-    purchases = pullPurchases(''.join(pdfContent))
+    purchases = ''.join(pdfContent).split("\n")
 
+    patterns = {"Fith/Third" : re.compile(r"\d{2}/\d{2}\s+\d+\.\d+.*?(?=X{12}\d{3}[X\d])")}
+    
+    foundPurchases = []
+    for textContent in purchases:
+        found = patterns["Fith/Third"].findall(textContent)
 
-    for x in purchases:
-        print(f'{x}\n\n')
-
-
-
-def pullPurchases(text: str) -> list:
-    textArray = text.split("\n")
-
-    foundPurchases = [content for content in textArray if isPurchase(content)]
-
+        for x in found: foundPurchases.append(x)
 
     return foundPurchases
-
-def isPurchase(content: str) -> bool:
-    confidence = 0.0
-    keyWords = {'transaction', 'debit', 'withdrawal', 'withdrawals/debits', 'withdrawals/debits-continueddate', 'payments'} # <-- make non static (move to json)
-
-    for word in content.split(" "):
-        if word.lower() in keyWords: confidence += 10.0
-        if isDate(word): confidence += 5.0
-
-    return confidence >= PaymentDefinitionConfidence
-
-
 
 def categorisePurchase(input):
     return [Purchase(15.77, "shopping"), Purchase(24.00, "misc"), Purchase(48.00, "misc")]
